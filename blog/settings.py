85a0c9e8d1ec366ec1b67287f31096b278a1ece3
Soham-047 <129654959+Soham-047@users.noreply.github.com>
2025-08-08 13:18:31 +0530

    dATABASE CONFIGURAITON

--- blog/settings.py
+++ blog/settings.py
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASE_URL = (
 #   'postgresql://neondb_owner:npg_8FdIwi1KcgTV@ep-winter-bird-a1pcahjs-pooler.ap-southeast-1.aws.neon.tech/neondb'
